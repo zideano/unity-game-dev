50cc7144732ebe6eaeed9c24c5be8c667446701e
Jermaine <jermaineandrethomas@gmail.com>
2018-03-22 15:05:34 +0000

    Create GameManager.cs

---
+++ GameManager.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+	private int selectedZombiePosition = 0;
+
+	public GameObject selectedZombie;
+	public List<GameObject> zombies;
+
+	// Store vector orientation of zombies
+	public Vector3 selectedSize;
+	public Vector3 defaultSize;
+
+	// Use this for initialization
+	void Start () {
+		SelectZombie (selectedZombie);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("left")) {
+			GetZombieLeft ();
+		}
+
+		if (Input.GetKeyDown("right")) {
+			GetZombieRight ();
+		}
+
+		if (Input.GetKeyDown("up")) {
+			PushUp ();
+		}
+	}
+
+	void GetZombieLeft() {
+		if (selectedZombiePosition == 0) {
+			selectedZombiePosition = 3;
+			SelectZombie (zombies[3]);
+		} else {
+			selectedZombiePosition = selectedZombiePosition - 1;
+			GameObject newZombie = zombies[selectedZombiePosition];
+			SelectZombie (newZombie);
+		}
+	}
+
+	void GetZombieRight() {
+		if (selectedZombiePosition == 3) {
+			selectedZombiePosition = 0;
+			SelectZombie (zombies[0]);
+		} else {
+			selectedZombiePosition = selectedZombiePosition + 1;
+			SelectZombie(zombies[selectedZombiePosition]);
+		}
+	}
+
+	void SelectZombie(GameObject newZombie) {
+		selectedZombie.transform.localScale = defaultSize;
+		selectedZombie = newZombie;
+		newZombie.transform.localScale = selectedSize;
+	}
+
+	void PushUp() {
+		Rigidbody rb = selectedZombie.GetComponents<Rigidbody> ();
+		rb.AddForce (0,0,10, ForceMode.Impulse);
+	}
+}
